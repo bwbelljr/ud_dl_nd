456fa544108706c56d8f0d9195c0eeb31f1fbacb
bwbelljr <bwbelljr@gmail.com>
2020-11-16 12:29:33 +0100

    feat: fix backprop method to pass unit tests

--- 2_neural_networks/project1/my_answers.py
+++ 2_neural_networks/project1/my_answers.py
@@ -65,7 +65,7 @@ class NeuralNetwork(object):
         # TODO: Hidden layer = Replace these values with your calculations.
 
         # Signals into hidden layer
-        hidden_inputs = X.dot(weights_input_to_hidden)
+        hidden_inputs = X.dot(self.weights_input_to_hidden)
 
         # Signals from hidden layer
         hidden_outputs = self.activation_function(hidden_inputs)
@@ -73,7 +73,7 @@ class NeuralNetwork(object):
         # TODO: Output layer - Replace these values with your calculations.
 
         # Signals into final output layer
-        final_inputs = hidden_outputs.dot(weights_hidden_to_output)
+        final_inputs = hidden_outputs.dot(self.weights_hidden_to_output)
 
         # Signals from final output layer
         final_outputs = final_inputs
@@ -92,29 +92,81 @@ class NeuralNetwork(object):
         delta_weights_h_o: change in weights from hidden to output layers
 
         '''
+        # In a few places, this post on Knowledge (https://knowledge.udacity.com/questions/178799) helped me with numpy array sizes
+
         #### Implement the backward pass here ####
         ### Backward pass ###
 
-        # TODO: Output error - Replace this value with your calculations
+        # TODO: Output error - Replace this value with your calculations.
+        error = y - final_outputs # Output layer error is the difference between desired target and actual output.
 
-        # Output layer error is the difference between desired target and
-        # actual output
-        error = y - final_outputs
+        # TODO: Backpropagated error terms - Replace these values with your calculations.
+        output_error_term = error
 
         # TODO: Calculate the hidden layer's contribution to the error
-        hidden_error = self.weights_hidden_to_output*error
-
-        # TODO: Backpropagated error terms - Replace these values with your
-        #       calculations
-        output_error_term = error
+        hidden_error = output_error_term.dot(self.weights_hidden_to_output.T)
 
         hidden_error_term = hidden_error*hidden_outputs*(1-hidden_outputs)
 
         # Weight step (input to hidden)
-        delta_weights_i_h += hidden_error_term*X
-        # Alternative is hidden_error_term[None, :]*x[:, None]
 
-        # Weight_step (hidden to output)
-        delta_weights_h_o += output_error_term*hidden_outputs
+        delta_weights_i_h += X[:, None]*hidden_error_term
+        #delta_weights_i_h += hidden_error_term[None, :]*X[:, None]
+
+        # Weight step (hidden to output)
+
+        delta_weights_h_o += hidden_outputs[:,None]*output_error_term
 
         return delta_weights_i_h, delta_weights_h_o
+
+
+    def update_weights(self, delta_weights_i_h, delta_weights_h_o, n_records):
+        ''' Update weights on gradient descent step
+
+            Arguments
+            ---------
+            delta_weights_i_h: change in weights from input to hidden layers
+            delta_weights_h_o: change in weights from hidden to output layers
+            n_records: number of records
+        '''
+        # update hidden-to-output weights with gradient descent step
+        self.weights_hidden_to_output += self.lr*delta_weights_h_o/n_records
+
+        # update input-to-hidden weights with gradient descent step
+        self.weights_input_to_hidden += self.lr*delta_weights_i_h/n_records
+
+    def run(self, features):
+        ''' Run a forward pass through the network with input features
+
+            Arguments
+            ---------
+            features: 1D array of feature values
+        '''
+
+        #### Implement the forward pass here ####
+        # TODO: Hidden layer - replace these values with the appropriate
+        #       calculations
+
+        # Signals into hidden layer
+        hidden_inputs = features.dot(self.weights_input_to_hidden)
+
+        # Signals from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        # TODO: Output layer - Replace these values with the appropriate
+        #       calculations
+        # Signals into final output layers
+        final_inputs = hidden_outputs.dot(self.weights_hidden_to_output)
+
+        # Signals from final output layers
+        final_outputs = final_inputs
+
+        return final_outputs
+
+###########################################
+# Set your hyperparameters here
+###########################################
+iterations = 100
+learning_rate = 0.1
+hidden_nodes = 2
+output_nodes = 1
